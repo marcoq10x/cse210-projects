d540d70b58f5fe803c4a2b2c6737f3df61afefa3
Use your GitHub username <Enter your email address>
2023-01-10 21:34:00 -0500

    Prep 2 C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask the user for their grade percentage
+        Console.WriteLine("Please enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // Determine the letter grade
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign of the grade
+        string sign = "";
+        if (grade % 10 >= 7 && letter != "A")
+        {
+            sign = "+";
+        }
+        else if (grade % 10 < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        // Handle the exceptional cases of A+ and F-
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        else if (letter == "F" && sign == "-")
+        {
+            sign = "";
+        }
+
+        // Print the grade letter and sign
+        Console.WriteLine($"{letter}{sign}");
+
+        // Determine if the user passed the course
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class. Now let's go party!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, better luck next time!");
+        }
     }
 }
